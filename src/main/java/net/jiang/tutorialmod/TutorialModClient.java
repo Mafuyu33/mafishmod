d3e8fd0e9b4cdac4d32ddc78fb565c3de0d59f08
Zhehan Jiang <ssyzj3@nottingham.ac.uk>
2023-12-10 16:33:11 +0000

    Day1

---
+++ src/main/java/net/jiang/tutorialmod/TutorialModClient.java
@@ -0,0 +1,7 @@
+package net.jiang.tutorialmod;
+import net.fabricmc.api.ClientModInitializer;
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
