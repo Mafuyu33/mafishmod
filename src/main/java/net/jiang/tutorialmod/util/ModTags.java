f30a2f0df98ce7a11c8d5eee8f5196da0ce31864
Mafuyu33 <jzh654354772@gmail.com>
2023-12-20 22:06:42 +0800

    Day5 Tags

--- src/main/java/net/jiang/tutorialmod/util/ModTags.java
+++ src/main/java/net/jiang/tutorialmod/util/ModTags.java
@@ -3,13 +3,12 @@ package net.jiang.tutorialmod.util;
 import net.jiang.tutorialmod.TutorialMod;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public class ModTags {
-    public static class Blocks{
+    public static class Blocks {
         public static final TagKey<Block> METAL_DETECTOR_DETECTABLE_BLOCKS =
                 createTag("metal_detector_detectable_blocks");
 
@@ -18,7 +17,7 @@ public class ModTags {
         }
     }
 
-    public static class Items{
+    public static class Items {
         private static TagKey<Item> createTag(String name){
             return TagKey.of(RegistryKeys.ITEM,new Identifier(TutorialMod.MOD_ID,name));
         }
