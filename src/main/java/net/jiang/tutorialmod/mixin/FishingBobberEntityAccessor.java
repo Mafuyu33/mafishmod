22e71f7f386da43cdddcff934fe57c4778a643f1
Mafuyu33 <jzh654354772@gmail.com>
2024-01-27 20:24:44 +0000

    access-widener习得，Mixin可以调用源文件中的私有方法了

---
+++ src/main/java/net/jiang/tutorialmod/mixin/FishingBobberEntityAccessor.java
@@ -0,0 +1,16 @@
+//package net.jiang.tutorialmod.mixin;
+//
+//
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.gen.Accessor;
+//import net.minecraft.entity.projectile.FishingBobberEntity;
+//import net.minecraft.entity.projectile.FishingBobberEntity.State;
+//
+//@Mixin(FishingBobberEntity.class)
+//public interface FishingBobberEntityAccessor<State> {
+//    @Accessor("state")
+//    State getState();
+//
+//    @Accessor("state")
+//    void setState(State state);
+//}
--- src/main/java/net/jiang/tutorialmod/mixin/FishingBobberMixin.java
+++ src/main/java/net/jiang/tutorialmod/mixin/FishingBobberMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
@@ -22,6 +23,7 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -39,8 +41,9 @@ public abstract class FishingBobberMixin extends ProjectileEntity {
 	}
 	@Unique
 	private int delayTimer = 0;
+
 	@Inject(at = @At("HEAD"), method = "tick")
-	private void init(CallbackInfo info) {
+	private void init(CallbackInfo info) {//冰霜行者
 		PlayerEntity playerEntity = getPlayerOwner();
 		if (playerEntity != null) {
 			ItemStack itemStack = playerEntity.getMainHandStack();
@@ -64,7 +67,7 @@ public abstract class FishingBobberMixin extends ProjectileEntity {
 		}
 	}
 	@Inject(at = @At("HEAD"), method = "tick")
-	private void init1(CallbackInfo info) {
+	private void init1(CallbackInfo info) {//海之厌恶
 		PlayerEntity playerEntity = getPlayerOwner();
 		if (playerEntity != null) {
 			ItemStack itemStack = playerEntity.getMainHandStack();
@@ -75,10 +78,62 @@ public abstract class FishingBobberMixin extends ProjectileEntity {
 					BlockPos blockPos = this.getBlockPos();
 					FluidState fluidState = world.getFluidState(blockPos);
 					if (fluidState.isIn(FluidTags.WATER)) {
-						this.addVelocity(0,1,0);
+					this.addVelocity(0,1,0);
 					}
 				}
 			}
 		}
 	}
+	/**
+	 * @author
+	 * Mafuyu33
+	 * @reason
+	 * 添加了力量附魔
+	 */
+	@Overwrite
+	public void pullHookedEntity(Entity entity) {//力量
+		float power_enchantment_level = 1;
+		Entity entity2 = this.getOwner();
+		if (entity2 instanceof PlayerEntity) {
+			ItemStack itemStack = ((PlayerEntity) entity2).getMainHandStack();
+			int k = EnchantmentHelper.getLevel(Enchantments.POWER, itemStack);
+			if (k > 0) {
+				power_enchantment_level = 1.0f + k * 0.5f;
+			}
+		}
+		if (entity2 != null) {
+			Vec3d vec3d = (new Vec3d(entity2.getX() - this.getX(), entity2.getY() - this.getY(), entity2.getZ() - this.getZ())).multiply(0.1*power_enchantment_level);
+			entity.setVelocity(entity.getVelocity().add(vec3d));
+		}
+	}
+	@Shadow private int hookCountdown;
+	@Shadow private int fishTravelCountdown;
+    @Shadow private FishingBobberEntity.State state;
+
+	@Inject(at = @At("TAIL"), method = "tick")
+	private void init2(CallbackInfo info) {//火焰保护
+		float f = 0.0F;
+		PlayerEntity playerEntity = getPlayerOwner();
+		if (playerEntity != null) {
+			ItemStack itemStack = playerEntity.getMainHandStack();
+			if(itemStack.getItem() == Items.FISHING_ROD) {
+				int k = EnchantmentHelper.getLevel(Enchantments.FIRE_PROTECTION, itemStack);
+				if (k > 0) {
+					World world = this.getWorld();
+					BlockPos blockPos = this.getBlockPos();
+					FluidState fluidState = world.getFluidState(blockPos);
+					if (fluidState.isIn(FluidTags.LAVA)) {
+						f = fluidState.getHeight(this.getWorld(), blockPos);
+					}
+					boolean bl = f > 0.0F;
+
+					if (bl) {
+						state= FishingBobberEntity.State.BOBBING;
+					}
+				}
+			}
+		}
+	}
+
+
 }
