f74ca42ca9d257107204f77f541e50ed22afd38e
Mafuyu33 <jzh654354772@gmail.com>
2024-01-28 10:08:43 +0000

    新的钓鱼竿附魔（火焰附加）可以在岩浆里钓鱼

--- src/main/java/net/jiang/tutorialmod/mixin/FishingBobberEntityMixin.java
+++ src/main/java/net/jiang/tutorialmod/mixin/FishingBobberEntityMixin.java
@@ -1,14 +1,11 @@
 package net.jiang.tutorialmod.mixin;
 
 import net.jiang.tutorialmod.enchantment.ModEnchantments;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
@@ -16,8 +13,7 @@ import net.minecraft.fluid.FluidState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.registry.tag.FluidTags;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -30,13 +26,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Iterator;
-
 @Mixin(FishingBobberEntity.class)
-public abstract class FishingBobberMixin extends ProjectileEntity {
+public abstract class FishingBobberEntityMixin extends ProjectileEntity {
 	@Shadow @Nullable public abstract PlayerEntity getPlayerOwner();
+	@Shadow
+	protected abstract void tickFishingLogic(BlockPos pos);
 
-	public FishingBobberMixin(EntityType<? extends ProjectileEntity> entityType, World world) {
+	public FishingBobberEntityMixin(EntityType<? extends ProjectileEntity> entityType, World world) {
 		super(entityType, world);
 	}
 	@Unique
@@ -45,10 +41,12 @@ public abstract class FishingBobberMixin extends ProjectileEntity {
 	@Inject(at = @At("HEAD"), method = "tick")
 	private void init(CallbackInfo info) {//冰霜行者
 		PlayerEntity playerEntity = getPlayerOwner();
-		if (playerEntity != null) {
-			ItemStack itemStack = playerEntity.getMainHandStack();
-			if(itemStack.getItem() == Items.FISHING_ROD) {
-				int k = EnchantmentHelper.getLevel(Enchantments.FROST_WALKER, itemStack);
+		if (playerEntity!=null) {
+			Hand hand = playerEntity.getActiveHand();
+			if (hand != null) {
+				ItemStack itemStack = playerEntity.getStackInHand(hand);
+				if (itemStack.getItem() == Items.FISHING_ROD) {
+					int k = EnchantmentHelper.getLevel(Enchantments.FROST_WALKER, itemStack);
 				if (k > 0) {
 					World world = this.getWorld();
 					BlockPos blockPos = this.getBlockPos();
@@ -63,22 +61,26 @@ public abstract class FishingBobberMixin extends ProjectileEntity {
 						}
 					}
 				}
+				}
 			}
 		}
 	}
 	@Inject(at = @At("HEAD"), method = "tick")
 	private void init1(CallbackInfo info) {//海之厌恶
 		PlayerEntity playerEntity = getPlayerOwner();
-		if (playerEntity != null) {
-			ItemStack itemStack = playerEntity.getMainHandStack();
-			if(itemStack.getItem() == Items.FISHING_ROD) {
-				int k = EnchantmentHelper.getLevel(ModEnchantments.BAD_LUCK_OF_SEA, itemStack);
-				if (k > 0) {
-					World world = this.getWorld();
-					BlockPos blockPos = this.getBlockPos();
-					FluidState fluidState = world.getFluidState(blockPos);
-					if (fluidState.isIn(FluidTags.WATER)) {
-					this.addVelocity(0,1,0);
+		if (playerEntity!=null) {
+			Hand hand = playerEntity.getActiveHand();
+			if (hand != null) {
+				ItemStack itemStack = playerEntity.getStackInHand(hand);
+				if (itemStack.getItem() == Items.FISHING_ROD) {
+					int k = EnchantmentHelper.getLevel(ModEnchantments.BAD_LUCK_OF_SEA, itemStack);
+					if (k > 0) {
+						World world = this.getWorld();
+						BlockPos blockPos = this.getBlockPos();
+						FluidState fluidState = world.getFluidState(blockPos);
+						if (fluidState.isIn(FluidTags.WATER)) {
+							this.addVelocity(0, 1, 0);
+						}
 					}
 				}
 			}
@@ -110,14 +112,15 @@ public abstract class FishingBobberMixin extends ProjectileEntity {
 	@Shadow private int fishTravelCountdown;
     @Shadow private FishingBobberEntity.State state;
 
+
 	@Inject(at = @At("TAIL"), method = "tick")
-	private void init2(CallbackInfo info) {//火焰保护
+	private void init2(CallbackInfo info) {//火焰附加，能在岩浆里钓鱼
 		float f = 0.0F;
 		PlayerEntity playerEntity = getPlayerOwner();
 		if (playerEntity != null) {
 			ItemStack itemStack = playerEntity.getMainHandStack();
 			if(itemStack.getItem() == Items.FISHING_ROD) {
-				int k = EnchantmentHelper.getLevel(Enchantments.FIRE_PROTECTION, itemStack);
+				int k = EnchantmentHelper.getLevel(Enchantments.FIRE_ASPECT, itemStack);
 				if (k > 0) {
 					World world = this.getWorld();
 					BlockPos blockPos = this.getBlockPos();
@@ -128,11 +131,20 @@ public abstract class FishingBobberMixin extends ProjectileEntity {
 					boolean bl = f > 0.0F;
 
 					if (bl) {
-						state= FishingBobberEntity.State.BOBBING;
+//						this.addVelocity(0,0.03,0);
+						this.setVelocity(0, 0.1, 0);
+						this.state = FishingBobberEntity.State.BOBBING;
+
+						if (!this.getWorld().isClient) {
+							this.tickFishingLogic(blockPos);
+						}
 					}
 				}
 			}
 		}
+//		if(this.state == FishingBobberEntity.State.BOBBING &playerEntity!=null){
+//			playerEntity.sendMessage(Text.literal((String.valueOf(123))),false);
+//		};//进漂浮状态的测试
 	}
 
 
