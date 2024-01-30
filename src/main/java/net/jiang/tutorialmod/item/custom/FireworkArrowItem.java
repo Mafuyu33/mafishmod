1ed17baa057b9238d67f533e0ad0b2c970411923
Mafuyu33 <jzh654354772@gmail.com>
2024-01-30 15:56:09 +0000

    机动飞爪！钓鱼竿冲击附魔

--- src/main/java/net/jiang/tutorialmod/item/custom/FireworkArrowItem.java
+++ src/main/java/net/jiang/tutorialmod/item/custom/FireworkArrowItem.java
@@ -6,18 +6,13 @@ import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
-import net.minecraft.item.ArrowItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.world.World;
 
-public class FireworkArrowItem extends ArrowItem {
-    public FireworkArrowItem(Item.Settings settings) {
+public class FireworkArrowItem extends TippedArrowItem {
+
+    public FireworkArrowItem(Settings settings) {
         super(settings);
-    }
-    public PersistentProjectileEntity createArrow(World world, ItemStack stack, LivingEntity shooter) {
-        return new FireworkArrowEntity(world, shooter);
     }
 }
--- src/main/java/net/jiang/tutorialmod/mixin/FishingRodItemMixin.java
+++ src/main/java/net/jiang/tutorialmod/mixin/FishingRodItemMixin.java
@@ -2,6 +2,7 @@ package net.jiang.tutorialmod.mixin;
 
 import net.jiang.tutorialmod.enchantment.ModEnchantments;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
@@ -10,9 +11,11 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.item.*;
 import net.minecraft.registry.tag.FluidTags;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -28,7 +31,33 @@ public abstract class FishingRodItemMixin extends Item implements Vanishable {
 	}
 	@Inject(at = @At("HEAD"), method = "use")
 	private void init(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+		ItemStack itemStack = user.getStackInHand(hand);
+		if (user.fishHook != null) {
+			int k = EnchantmentHelper.getLevel(Enchantments.PUNCH, itemStack);//冲击附魔
+			if (k > 0) {
+//				//发送信息给玩家
+//				user.sendMessage(Text.literal((String.valueOf(k))),false);
 
+				Vec3d fishHookPos = user.fishHook.getPos();
+				Vec3d userPos = user.getPos();
+
+				// 计算朝向鱼钩的方向向量
+				Vec3d direction = fishHookPos.subtract(userPos).normalize();
+
+				// 设定速度大小（可以根据需要调整）
+				double speed = 1 + k * 0.5;
+
+				// 计算最终的速度向量
+				Vec3d velocity = direction.multiply(speed);
+//				//发送信息给玩家
+//				user.sendMessage(Text.literal((String.valueOf(velocity))),false);
+
+				// 将速度应用到玩家身上
+				user.addVelocity(velocity);
+
+
+			}
+		}
 	}
 
 }
