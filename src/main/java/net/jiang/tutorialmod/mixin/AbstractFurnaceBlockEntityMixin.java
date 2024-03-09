146f20b410d36b1900590596686b1e3d1ae7a444
Mafuyu33 <jzh654354772@gmail.com>
2024-03-09 16:03:41 +0000

    火焰附加熔炉

--- src/main/java/net/jiang/tutorialmod/mixin/AbstractFurnaceBlockEntityMixin.java
+++ src/main/java/net/jiang/tutorialmod/mixin/AbstractFurnaceBlockEntityMixin.java
@@ -1,43 +1,39 @@
 package net.jiang.tutorialmod.mixin;
 
 import net.jiang.tutorialmod.mixinhelper.BlockEnchantmentHelper;
-import net.minecraft.block.AbstractFurnaceBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.entity.LockableContainerBlockEntity;
 import net.minecraft.enchantment.Enchantments;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.SidedInventory;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtList;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.RecipeEntry;
 import net.minecraft.recipe.RecipeInputProvider;
 import net.minecraft.recipe.RecipeUnlocker;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Objects;
-
-import static net.minecraft.block.entity.AbstractFurnaceBlockEntity.createFuelTimeMap;
 
 @Mixin(AbstractFurnaceBlockEntity.class)
 public abstract class AbstractFurnaceBlockEntityMixin extends LockableContainerBlockEntity implements SidedInventory, RecipeUnlocker, RecipeInputProvider {
-	protected AbstractFurnaceBlockEntityMixin(BlockEntityType<?> blockEntityType, BlockPos blockPos, BlockState blockState) {
+	protected AbstractFurnaceBlockEntityMixin(BlockEntityType<?> blockEntityType, BlockPos blockPos, BlockState blockState, Boolean isFortune) {
 		super(blockEntityType, blockPos, blockState);
 	}
-
 	@Inject(at = @At("HEAD"), method = "getFuelTime",cancellable = true)
-	private void init(ItemStack fuel, CallbackInfoReturnable<Integer> cir) {
+	private void init1(ItemStack fuel, CallbackInfoReturnable<Integer> cir) {
 		int k = BlockEnchantmentHelper.getLevel(Enchantments.FIRE_ASPECT,pos);
-		System.out.println(k);
-		if(k>0){
+		if(k>0){//火焰附加
 			BlockPos firePos =pos;
 			firePos = firePos.add(0,1,0);
 			if(world!=null && !world.isClient) {
