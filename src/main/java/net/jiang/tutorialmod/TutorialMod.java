53aacb822e25c2508e72db921352d8dabc9f44a1
Mafuyu33 <jzh654354772@gmail.com>
2024-03-26 21:33:36 +0000

    更改了包名

--- src/main/java/net/jiang/tutorialmod/TutorialMod.java
+++ src/main/java/net/jiang/tutorialmod/TutorialMod.java
@@ -22,7 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID="tutorialmod";
+	public static final String MOD_ID="mafishmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
