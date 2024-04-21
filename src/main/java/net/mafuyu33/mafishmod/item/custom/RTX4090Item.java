cbc8805f7ecc6123d8864016fd2ce3e3efc6d19b
Mafuyu33 <jzh654354772@gmail.com>
2024-04-21 13:14:32 +0100

    4090矿卡（没做完）

--- src/main/java/net/mafuyu33/mafishmod/item/custom/RTX4090Item.java
+++ src/main/java/net/mafuyu33/mafishmod/item/custom/RTX4090Item.java
@@ -1,6 +1,7 @@
 package net.mafuyu33.mafishmod.item.custom;
 
 import net.mafuyu33.mafishmod.entity.FuProjectileEntity;
+import net.mafuyu33.mafishmod.sound.ModSounds;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.PickaxeItem;
@@ -24,19 +25,7 @@ public class RTX4090Item extends PickaxeItem {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
         world.playSound(null, user.getX(), user.getY(), user.getZ(),
-                SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
-
-        if (!world.isClient) {
-            FuProjectileEntity fuProjectileEntity = new FuProjectileEntity(user, world);
-            fuProjectileEntity.setItem(itemStack);
-            fuProjectileEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0f, 1.5f, 1.0f);
-            world.spawnEntity(fuProjectileEntity);
-        }
-
-        user.incrementStat(Stats.USED.getOrCreateStat(this));
-        if (!user.getAbilities().creativeMode) {
-            itemStack.decrement(1);
-        }
+                ModSounds.PIN, SoundCategory.PLAYERS, 0.5f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
         return TypedActionResult.success(itemStack, world.isClient());
     }
 }
