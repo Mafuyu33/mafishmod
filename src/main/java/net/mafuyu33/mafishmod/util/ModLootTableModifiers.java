61f5d69fe923c9ca3e24d77405f8262cacf271de
Mafuyu33 <jzh654354772@gmail.com>
2024-06-29 17:37:29 +0800

    战利品表添加，可以在废弃传送门开出

--- src/main/java/net/mafuyu33/mafishmod/util/ModLootTableModifiers.java
+++ src/main/java/net/mafuyu33/mafishmod/util/ModLootTableModifiers.java
@@ -1,16 +1,26 @@
 package net.mafuyu33.mafishmod.util;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.mafuyu33.mafishmod.block.ModBlocks;
+import net.mafuyu33.mafishmod.item.ModItemGroups;
 import net.mafuyu33.mafishmod.item.ModItems;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
+import java.util.List;
+import java.util.Random;
+
 public class ModLootTableModifiers {
+    private static final Identifier MOD_ITEM_ID =
+            new Identifier("minecraft", "chests/ruined_portal");
     private static final Identifier JUNGLE_TEMPLE_ID =
             new Identifier("minecraft", "chests/jungle_temple");
     private static final Identifier CREEPER_ID =
@@ -18,6 +28,43 @@ public class ModLootTableModifiers {
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if(MOD_ITEM_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f)) // Drops 100% of the time
+                        .with(ItemEntry.builder(ModItems.RUBY))
+
+                        .rolls(ConstantLootNumberProvider.create(2))
+                        .conditionally(RandomChanceLootCondition.builder(1f)) // Drops 100% of the time
+                        .with(ItemEntry.builder(ModItems.TIME_STOP))
+
+                        .rolls(ConstantLootNumberProvider.create(3))
+                        .conditionally(RandomChanceLootCondition.builder(1f)) // Drops 100% of the time
+                        .with(ItemEntry.builder(ModItems.MATH_SWORD))
+
+                        .rolls(ConstantLootNumberProvider.create(4))
+                        .conditionally(RandomChanceLootCondition.builder(1f)) // Drops 100% of the time
+                        .with(ItemEntry.builder(ModItems.COLLIABLE))
+
+                        .rolls(ConstantLootNumberProvider.create(5))
+                        .conditionally(RandomChanceLootCondition.builder(1f)) // Drops 100% of the time
+                        .with(ItemEntry.builder(ModItems.VR_PEN))
+
+                        .rolls(ConstantLootNumberProvider.create(6))
+                        .conditionally(RandomChanceLootCondition.builder(1f)) // Drops 100% of the time
+                        .with(ItemEntry.builder(ModItems.VR_RUBBER))
+
+                        .rolls(ConstantLootNumberProvider.create(7))
+                        .conditionally(RandomChanceLootCondition.builder(1f)) // Drops 100% of the time
+                        .with(ItemEntry.builder(ModItems.VR_RULER))
+
+                        .rolls(ConstantLootNumberProvider.create(8))
+                        .conditionally(RandomChanceLootCondition.builder(1f)) // Drops 100% of the time
+                        .with(ItemEntry.builder(ModItems.VR_COMPASSES))
+                         ;
+
+                tableBuilder.pool(poolBuilder.build());
+            }
             if(JUNGLE_TEMPLE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
