e3deb7e90c9f28b95575570612ce6f2763598ceb
Mafuyu33 <jzh654354772@gmail.com>
2024-07-22 20:35:27 +0800

    从生物实体碰撞升级为实体碰撞

---
+++ src/main/java/net/mafuyu33/mafishmod/mixin/itemmixin/colliableitem/EntityMixin.java
@@ -0,0 +1,46 @@
+package net.mafuyu33.mafishmod.mixin.itemmixin.colliableitem;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.mafuyu33.mafishmod.effect.ModStatusEffects;
+import net.mafuyu33.mafishmod.item.custom.ColliableItem;
+import net.mafuyu33.mafishmod.mixinhelper.WeaponEnchantmentMixinHelper;
+import net.mafuyu33.mafishmod.networking.ModMessages;
+import net.minecraft.entity.Attackable;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.passive.FoxEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+    @Inject(method = "isCollidable", at = @At("HEAD"),cancellable = true)
+    private void init(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(ColliableItem.isColliable());
+    }
+}
